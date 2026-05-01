2f3b363819c31cb877937a51f4fbbb2bdc3b91d4
ElionKelmendi <ek72871@ubt-uni.net>
2026-05-01 20:41:55 +0200

    Add files via upload
    Entitetet e backend per projekt

---
+++ Regjistrimi_workshop.cs
@@ -0,0 +1,16 @@
+namespace MyApp.Domain;
+
+public class Regjistrimi_Workshop
+{
+
+    public int rw_id {get; set; }
+
+    public int workshop_id {get; set; }
+
+    public int anetar_id {get; set; }
+
+    public DateTime data_regjistrimit {get; set; }
+
+    public Boolean statusi_pageses {get; set; }
+    
+}
---
+++ Workshop.cs
@@ -0,0 +1,23 @@
+namespace MyApp.Domain;
+
+public class Workshop
+{
+    public int workshop_id { get; set; }
+
+    public String titulli {get; set; }
+
+    public String pershkrimi {get; set; }
+
+    public int instruktor_id{get; set; }
+
+    public DateTime data {get; set; }
+
+    public DateTime ora_fillimit {get; set; }
+
+    public DateTime ora_perfundimit {get; set; }
+
+    public double cmimi {get; set; }
+
+    public kapaciteti {get; set; } 
+
+}
