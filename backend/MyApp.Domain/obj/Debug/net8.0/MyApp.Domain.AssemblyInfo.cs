1ca1c54b240f880a91295329fc0156c0aeb15cc8
Redo-on <redonkurtaj2006@gmail.com>
2026-05-05 14:26:52 +0200

    ja rregullova lokacionet

--- backend/MyApp.Domain/obj/Debug/net8.0/MyApp.Domain.AssemblyInfo.cs
+++ backend/MyApp.Domain/obj/Debug/net8.0/MyApp.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c32d8388fd2ea1b09bef2b8dc774729ee3b28800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f3b363819c31cb877937a51f4fbbb2bdc3b91d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
